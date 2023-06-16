0f590348870ac2bfdeef05ddf417195716a48eea
Shashwat Kapoor <shashwatquora2000@gmail.com>
2023-06-16 11:31:05 +0530

    customizing admin page

--- src/urls.py
+++ src/urls.py
@@ -18,6 +18,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('' , include('accounts.urls')),
+    path('jet/', include('jet.urls', 'jet')), 
     path('admin/', admin.site.urls),
     path('events/', include('events.urls')),
     path('noticeboard/', include('noticeBoard.urls')),
