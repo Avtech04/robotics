3a535c9c08c4b4d79e152b75eb62b68d1f8e8e67
Shashwat Kapoor <shashwatquora2000@gmail.com>
2023-06-17 22:41:58 +0530

    make design changes in articles

--- articles/models.py
+++ articles/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Article(models.Model):
-    title=models.CharField(max_length=20)
+    title=models.CharField(max_length=100)
     slug=models.SlugField()
     body=models.TextField()
     date=models.DateTimeField(auto_now_add=True)
