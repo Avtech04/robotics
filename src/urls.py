e8e7fd07d1d5558226691c792b8d0916d5eb47ba
Mickey <mickeyth59@gmail.com>
2023-06-17 09:09:27 +0530

    resolved image rendering in articles

--- src/urls.py
+++ src/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('' , include('accounts.urls')),
@@ -25,3 +27,5 @@ urlpatterns = [
     path('articles/', include('articles.urls'))
     
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
