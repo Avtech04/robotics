d7e4a6c4a5f301d191a39af2fc7f0f6dfa507509
Shashwat Kapoor <shashwatquora2000@gmail.com>
2023-06-17 09:46:20 +0530

    adding some functionalities

--- articles/forms.py
+++ articles/forms.py
@@ -5,3 +5,11 @@ class CreateArticle(forms.ModelForm):
     class Meta:
         model=models.Article
         fields=['title','body','slug','thumb']
+        
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'}),
+            'slug': forms.TextInput(attrs={'class': 'form-control'}),
+            'thumb': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+        }
+        
